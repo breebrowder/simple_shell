4ac1d6399efa4e71f02f1f2e37d07bb14ae38747
breezybrow <browder.bree@gmail.com>
2021-04-05 22:28:02 +0000

    adding concepts page

---
+++ testdirectory/getppid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        pid_t my_pid;
+
+        my_pid = getpid();
+        printf("child %u\n", my_pid);
+
+        my_pid = getppid();
+        printf("father %u\n", my_pid);
+        return (0);
+}
---
+++ testdirectory/prompt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- function that prints prompt, waits for user to enter command
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *buffer = malloc(1024);
+	size_t len = 1024;
+
+	while (1)
+	{
+		printf("$ ");
+		getline(&buffer, &len, stdin);
+		printf("%s", buffer);
+	}
+	return (0);
+}
